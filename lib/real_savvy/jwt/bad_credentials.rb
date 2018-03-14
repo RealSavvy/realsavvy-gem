8f252c1bfd6f21b5e9443a813d8867483087da0a
Andrew Rove <andrew@realsavvy.com>
2018-03-14 17:30:46 -0500

    Add JWT Token Support.
    Singularize markets’ class name.

---
+++ lib/real_savvy/jwt/bad_credentials.rb
@@ -0,0 +1,6 @@
+module RealSavvy
+  module JWT
+    class BadCredentials < StandardError
+    end
+  end
+end
---
+++ lib/real_savvy/jwt/site.rb
@@ -0,0 +1,9 @@
+module RealSavvy
+  module JWT
+    module Site
+      def is_real_savvy_site?
+        true
+      end
+    end
+  end
+end
