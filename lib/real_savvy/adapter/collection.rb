ecc0822bfe60d10e1a26a736cbdb3813e2992687
Andrew Rove <andrew@realsavvy.com>
2018-04-18 17:47:29 -0500

    Sorting Support

--- lib/real_savvy/adapter/collection.rb
+++ lib/real_savvy/adapter/collection.rb
@@ -16,15 +16,15 @@ class RealSavvy::Adapter::Collection < RealSavvy::Adapter::Base
     delete("./api/v3/#{path_prefix}/#{id}/remove", {property_ids: property_ids})
   end
 
-  def search(id:, filter: {}, market_id: nil, page_size: nil, page_number: nil, page: {})
+  def search(id:, market_id: nil, page_size: nil, page_number: nil, page: {}, sort: {}, filter: {})
     page[:size] ||= page_size
     page[:number] ||= page_number
-    post("./api/v3/#{path_prefix}/#{id}/properties/search", {filter: filter, market_id: market_id, page: page})
+    post("./api/v3/#{path_prefix}/#{id}/properties/search", {filter: filter, market_id: market_id, page: page, sort: sort})
   end
 
-  def cluster(id:, filter: {}, market_id: nil, page_size: nil, page_number: nil, page: {}, precision: nil)
+  def cluster(id:, market_id: nil, page_size: nil, page_number: nil, page: {}, sort: {}, filter: {}, precision: nil)
     page[:size] ||= page_size
     page[:number] ||= page_number
-    post("./api/v3/#{path_prefix}/#{id}/properties/map/clusters", {filter: filter, market_id: market_id, page: page, precision: precision})
+    post("./api/v3/#{path_prefix}/#{id}/properties/map/clusters", {filter: filter, market_id: market_id, page: page, sort: sort, precision: precision})
   end
 end
--- lib/real_savvy/concern/index_for_adapter.rb
+++ lib/real_savvy/concern/index_for_adapter.rb
@@ -1,7 +1,7 @@
 module RealSavvy::Concern::IndexForAdapter
-  def index(page_size: nil, page_number: nil, page: {})
+  def index(page_size: nil, page_number: nil, page: {}, sort: {}, filter: {})
     page[:size] ||= page_size
     page[:number] ||= page_number
-    get("./api/v3/#{path_prefix}", {page: page})
+    get("./api/v3/#{path_prefix}", {page: page, sort: sort, filter: filter})
   end
 end
